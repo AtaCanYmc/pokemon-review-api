eab3efc3e71d8ea1febaecff4050da34f199eb4f
Ata Can <atacanymc@gmail.com>
2023-11-24 03:08:03 +0300

    spring project packages created

---
+++ src/main/java/com/atacanymc/pokemonreviewapi/PokemonReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.atacanymc.pokemonreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonReviewApiApplication.class, args);
+    }
+
+}
