e68f62bd5ccaae9b0f465224abcabd4e0c43253d
Ata Can <atacanymc@gmail.com>
2023-11-24 06:22:51 +0300

    Main services created, first exceptions implemented.

--- src/main/java/com/atacanymc/pokemonreviewapi/DTOs/Converter/ReviewDtoConverter.java
+++ src/main/java/com/atacanymc/pokemonreviewapi/DTOs/Converter/ReviewDtoConverter.java
@@ -4,10 +4,14 @@ import com.atacanymc.pokemonreviewapi.DTOs.Response.Review.PokemonReviewDto;
 import com.atacanymc.pokemonreviewapi.DTOs.Response.Review.ReviewDto;
 import com.atacanymc.pokemonreviewapi.DTOs.Response.Review.UserReviewDto;
 import com.atacanymc.pokemonreviewapi.Model.Review;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class ReviewDtoConverter {
+    private final PokemonDtoConverter pokemonDtoConverter;
+    private final UserDtoConverter userDtoConverter;
     public ReviewDto convert(Review review) {
         return new ReviewDto(review.getId(),
                 review.getTitle(),
@@ -17,19 +21,19 @@ public class ReviewDtoConverter {
                 review.getReviewer().getId());
     }
 
-    public static UserReviewDto convertUserReview(Review review) {
+    public UserReviewDto convertUserReview(Review review) {
         return new UserReviewDto(review.getId(),
                 review.getTitle(),
                 review.getBody(),
                 review.getRating(),
-                PokemonDtoConverter.convert(review.getPokemon()));
+                pokemonDtoConverter.convert(review.getPokemon()));
     }
 
-    public static PokemonReviewDto convertPokemonReview(Review review) {
+    public PokemonReviewDto convertPokemonReview(Review review) {
         return new PokemonReviewDto(review.getId(),
                 review.getTitle(),
                 review.getBody(),
                 review.getRating(),
-                UserDtoConverter.convert(review.getReviewer()));
+                userDtoConverter.convert(review.getReviewer()));
     }
 }
